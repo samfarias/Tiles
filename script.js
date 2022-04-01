3f79eadd3fa476e5719356f0da091bdaa913f340
Sam <samfarias8@gmail.com>
2022-04-01 16:03:39 -0400

    update
    unworking working code

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let colorSelected;
+var babehs = document.getElementById("grid");
 
 //Adds a row
 function addR() {
@@ -15,14 +16,44 @@ function addR() {
         };
         row.appendChild(col);
         grid.appendChild(row);
-
+    }
+    else {
+        let cols = document.getElementsByTagName("td");
+        let row = document.createElement("tr");
+        let col = document.createElement("td");
+        col.onclick = function () {
+            this.style.backgroundColor = colorSelected;
+        };
+        row.appendChild(col);
+        grid.appendChild(row);
     }
 }
 //Adds a column
 function addC() {
     //alert("Clicked Add Col")
+    let grid = document.getElementById("grid");
+    let rows = document.getElementsByTagName("tr");
     let cols = document.getElementsByTagName("td");
-    console.log(cols.length);
+    let col = document.createElement("td");
+    let row = document.createElement("tr");
+
+    console.log("cols " + cols.length);
+    //we only add columns if rows is >= 1
+    //if (rows.length > 0)
+    //iterate through rows and insert a table
+//      function appendColumn() {
+        var tbl = document.getElementsByTagName("grid");
+        //console.log(tbl.rows() + "is row ")
+
+        for (let i = 0; i < rows.length; i++) {
+          grid[i].appendChild(col);
+        }
+        //for (const p of grid) {
+          //appendChild(col);
+        //}
+//      }
+      //if (rows.length > 0){appendColumn();}
+//    appendColumn();
 }
 
 //Removes a row
